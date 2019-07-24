46e3a2830e89b73e234f3754152169b99ec9de5c
Jorel <jorel.k.ali@gmail.com>
2019-07-25 00:37:54 +0100

    Adds glorious class loading system

--- AnnotationTest/src/main/java/io/github/jorelali/commandapi/safereflection/SafeReflectionProcessor.java
+++ AnnotationTest/src/main/java/io/github/jorelali/commandapi/safereflection/SafeReflectionProcessor.java
@@ -1,11 +1,18 @@
 package io.github.jorelali.commandapi.safereflection;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.JarURLConnection;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Enumeration;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.Messager;
@@ -14,6 +21,7 @@ import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.type.MirroredTypeException;
 import javax.tools.Diagnostic.Kind;
 
 import com.google.auto.service.AutoService;
@@ -71,24 +79,89 @@ public class SafeReflectionProcessor extends AbstractProcessor {
 	private void processSafeReflection(SafeReflection safeReflection) {
 		if(!safeReflection.field().equals("")) {
 			for(String version : safeReflection.versions()) {
-				checkValidField(version, safeReflection.field());
+				checkValidField(version, safeReflection.field(), getTargetName(safeReflection));
 			}
+		} else if(!safeReflection.method().equals("")) {
+			for(String version : safeReflection.versions()) {
+				checkValidMethod(version, safeReflection.method(), getTargetName(safeReflection));
+			}
+		} else {
+			messager.printMessage(Kind.ERROR, "Invalid SafeReflection field/method field");
 		}
-		for(String version : safeReflection.versions()) {
-			File lib = new File(root, version + ".txt");
-			if (!lib.exists()) {
-				messager.printMessage(Kind.ERROR, "Cannot find library123123!");
-			}
+	}
+	
+	private String getTargetName(SafeReflection safeReflection) {
+		try {
+			safeReflection.target();
+		} catch(MirroredTypeException e) {
+			return e.getTypeMirror().toString();
+		}
+		return null;
+	}
+	
+	public Class<?> getClass(String target, String version) throws IOException, ClassNotFoundException {
+		
+		//Load the file
+		File spigot = new File(root, "spigotlibs/spigot-" + version + ".jar");
+		JarFile jarFile = new JarFile(spigot);
+		Enumeration<JarEntry> e = jarFile.entries();
+
+		//Load the classloader for the file
+		URL[] urls = { new URL("jar:file:" + spigot + "!/") };
+		URLClassLoader cl = URLClassLoader.newInstance(urls);
+
+		String targetClass = target.replace(".", "/") + ".class";		
+		boolean found = false;
+		
+		while (e.hasMoreElements()) {
+		    JarEntry entry = e.nextElement();
+		    
+		    if(entry.getName().equals(targetClass)) {
+		    	found = true;
+		    	//print(entry.getName());
+		    	Arrays.stream(cl.loadClass(target).getDeclaredMethods()).forEach(o -> {
+		    		print(o.getName());
+		    	});
+		    	break;
+		    }
+		    
+//		    if(entry.isDirectory() && index != directories.length - 1) {
+//		    	//traverse deeper
+//		    	//entry.
+//		    }
+//		    
+//		    if(entry.isDirectory() || !entry.getName().endsWith(".class")){
+//		        continue;
+//		    }
+//		    // -6 because of .class
+//		    String className = entry.getName().substring(0,entry.getName().length()-6);
+//		    className = className.replace('/', '.');
+//		    Class c = cl.loadClass(className);
+
 		}
 		
+		jarFile.close();
+		return null;
 	}
 	
 	//false if it goes wrong
-	public boolean checkValidField(String version, String fieldName) {
+	public boolean checkValidField(String version, String fieldName, String target) {
+		try {
+			getClass(target, version);
+		} catch (ClassNotFoundException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return false;
 	}
 	
-	public boolean checkValidMethod(String version, String methodName) {
+	public boolean checkValidMethod(String version, String methodName, String target) {
+		try {
+			getClass(target, version);
+		} catch (ClassNotFoundException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		return false;
 	}
 	
