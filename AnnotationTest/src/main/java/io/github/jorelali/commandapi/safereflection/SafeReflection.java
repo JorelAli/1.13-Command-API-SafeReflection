7a190231fd332724786e208ef1b1079ed12ee9f0
Jorel <jorel.k.ali@gmail.com>
2019-07-25 01:03:52 +0100

    Changed target elementtype to TYPE instead of FIELD

--- AnnotationTest/src/main/java/io/github/jorelali/commandapi/safereflection/SafeReflection.java
+++ AnnotationTest/src/main/java/io/github/jorelali/commandapi/safereflection/SafeReflection.java
@@ -6,7 +6,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target(ElementType.FIELD)
+@Target(ElementType.TYPE)
 @Retention(RetentionPolicy.SOURCE)
 @Repeatable(SafeReflections.class)
 public @interface SafeReflection {
